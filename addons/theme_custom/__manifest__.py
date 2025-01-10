1eaee7a58cf57bae47dc0475ac19d50ae65a181c
xavier.zheng <xavier.zheng@zkteco.com>
2025-01-10 17:37:05 +0800

    custom theme

---
+++ addons/theme_custom/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Custom Theme',
+    'description': 'A custom theme for Odoo',
+    'version': '1.0',
+    'author': 'xavier',
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+        'views/layout.xml',
+    ],
+    'category': 'Theme/Creative',
+}
